bac884f86a53c45c2fa9569966ec8bf579fc90c4
davidcloudobi <davidcloudobi@gmail.com>
2020-01-03 08:53:10 +0100

    Updates

---
+++ precache-manifest.6f971aabff235ffcaa6ef139bc7e2b75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8acea1602a46b23af066ed698f2c3662",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a9be5e1ea06085da0c63",
+    "url": "/monsters-rolodex/static/css/main.2b8d54f5.chunk.css"
+  },
+  {
+    "revision": "f3e266864ed10189c0e9",
+    "url": "/monsters-rolodex/static/js/2.76e2ac4d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.76e2ac4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9be5e1ea06085da0c63",
+    "url": "/monsters-rolodex/static/js/main.98657737.chunk.js"
+  },
+  {
+    "revision": "b39f9c079c1faacde559",
+    "url": "/monsters-rolodex/static/js/runtime-main.d7ade7ee.js"
+  }
+]);
